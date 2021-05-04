68b7aa6d0005da6055e132d762793317a86c69e7
FlorangelRP <florangelrp26@gmail.com>
2021-05-04 18:34:23 -0400

    cambios de solicitudes de servicio, se agregaron vistas parciales comunes para los tipos de sevicios a usar.

---
+++ CustomerSupport/Models/MDetailTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerSupport.Models
+{
+    public class MDetailTask
+    {
+        public int IdTask { get; set; }
+        public int IdDetailTask { get; set; }
+
+    }
+}
---
+++ CustomerSupport/Models/MTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerSupport.Models
+{
+    public class MTask
+    {
+        public int IdTask { get; set; }
+        public int IdUser { get; set; }
+        public string Activity { get; set; }
+        public System.DateTime DateIni { get; set; }
+        public System.DateTime DateEnd { get; set; }
+        public System.TimeSpan HourIni { get; set; }
+        public System.TimeSpan HourEnd { get; set; }
+        public string Place { get; set; }
+        public List<MDetailTask> ListDetailTask { get; set; }
+
+    }
+}
