0e7446316dff56a391b6d7b1e06ae824ef7cc245
AnaLucena07 <caanagracielalu@gmail.com>
2021-06-11 19:00:21 -0400

    cambios en listar

--- CustomerSupport/Controllers/ServiceRequestController.cs
+++ CustomerSupport/Controllers/ServiceRequestController.cs
@@ -20,34 +20,7 @@ namespace CustomerSupport.Controllers
         {
             List<MServiceRequest> ListServiceRequest = new List<MServiceRequest>();
             MMEnterprisesEntities db = new MMEnterprisesEntities();
-
-            //ListServiceRequest = (from result in db.GNListServiceRequest(null).ToList()
-            //            select new MUser
-            //            {
-            //                IdServiceRequest = result.IdServiceRequest
-            //                //,PersonClient = ((MPerson)(from result2 in db.GNListPerson(result.IdPerson, null)
-            //                //                  select new MPerson
-            //                //                  {
-            //                //                      IdPerson = result2.IdPerson,
-            //                //                      IdPersonType = result2.IdPersonType,
-            //                //                      PersonType = result2.PersonType,
-            //                //                      IdIdentificationType = result2.IdIdentificationType,
-            //                //                      IdentificationType = result2.IdentificationType,
-            //                //                      NumIdentification = result2.NumIdentification,
-            //                //                      Name = result2.Name,
-            //                //                      LastName = result2.LastName,
-            //                //                      Birthday = result2.Birthday,
-            //                //                      Address = result2.Address,
-            //                //                      Email = result2.Email,
-            //                //                      IdContactType = result2.IdContactType,
-            //                //                      ContactType = result2.ContactType,
-            //                //                      IdPosition = result2.IdPosition,
-            //                //                      Position = result2.Position,
-            //                //                      ClientPermission = result2.ClientPermission,
-            //                //                      Status = result2.Status
-            //                //                  }))
-            //            }).ToList();
-
+            ListServiceRequest = fnListServiceRequest(null, null,null,null,null);
             return Json(ListServiceRequest, JsonRequestBehavior.AllowGet);
 
         }
@@ -56,5 +29,49 @@ namespace CustomerSupport.Controllers
         {
             return View();
         }
+
+        public static List<MServiceRequest> fnListServiceRequest(int? IdServiceRequest, int? IdServiceType, int? IdServiceStatus = null, int? IdPerson = null, int? IdUser = null)
+        {
+            List<MServiceRequest> ListServiceRequest = new List<MServiceRequest>();
+            MMEnterprisesEntities db = new MMEnterprisesEntities();
+
+             ListServiceRequest = (from d in db.GNListServiceRequest(IdServiceRequest, IdServiceType, IdServiceStatus, IdPerson, IdUser).ToList()
+            join c in db.VWListCatalog.Where(t => t.IdTable == "SERVICESTATUS").ToList() on d.IdServiceStatus equals c.IdCatalogDetail
+                        select new MServiceRequest
+                          {
+                              IdServiceRequest = d.IdServiceRequest,
+                              IdServiceType = d.IdServiceType,
+                              ServiceType = d.ServiceType,
+                              IdServiceStatus = d.IdServiceStatus,
+                              ServiceStatus = c.DetailDesc,
+                              IdPerson = d.IdPerson,
+                              PersonClient = (MPerson)(from result2 in db.GNListPerson(d.IdPerson, null, null).ToList()
+                                            select new MPerson
+                                            {
+                                                IdPerson = result2.IdPerson,
+                                                IdPersonType = result2.IdPersonType,
+                                                PersonType = result2.PersonType,
+                                                IdIdentificationType = result2.IdIdentificationType,
+                                                IdentificationType = result2.IdentificationType,
+                                                NumIdentification = result2.NumIdentification,
+                                                Name = result2.Name,
+                                                LastName = result2.LastName,
+                                                Birthday = result2.Birthday,
+                                                Address = result2.Address,
+                                                Email = result2.Email,
+                                                IdContactType = result2.IdContactType,
+                                                ContactType = result2.ContactType,
+                                                IdPosition = result2.IdPosition,
+                                                Position = result2.Position,
+                                                ClientPermission = result2.ClientPermission,
+                                                Status = result2.Status
+                                            }).ToList().First(),
+                                     RegisterDate = d.RegisterDate
+                                 }).ToList();
+
+            return ListServiceRequest;
+
+        }
+
     }
 }
