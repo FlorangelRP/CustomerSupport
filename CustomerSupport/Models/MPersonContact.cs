e58e4ed3577c243d9217d70d478c66a2b4a89344
AnaLucena07 <caanagracielalu@gmail.com>
2021-07-12 21:24:07 -0400

    Cambios para encriptar la información del cliente

--- CustomerSupport/Models/MPersonContact.cs
+++ CustomerSupport/Models/MPersonContact.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 
 namespace CustomerSupport.Models
@@ -15,5 +17,25 @@ namespace CustomerSupport.Models
         public string CountryAreaCode { get; set; } //codigo de area segun el pais seleccionado
         public string PhoneNumber { get; set; }
         public bool Status { get; set; }
+
+        public string Desencriptar(string input)
+        {
+            try
+            {
+                byte[] iv = ASCIIEncoding.ASCII.GetBytes("qualityi");
+                byte[] encryptionKey = Convert.FromBase64String("rpadftlyhorfdertghyujki8765rgyhj");
+                byte[] buffer = Convert.FromBase64String(input);
+                TripleDESCryptoServiceProvider des = new TripleDESCryptoServiceProvider();
+                des.Key = encryptionKey;
+                des.IV = iv;
+                return Encoding.UTF8.GetString(des.CreateDecryptor().TransformFinalBlock(buffer, 0, buffer.Length));
+            }
+            catch (Exception)
+            {
+                return input;
+            }
+
+        }
+
     }
 }
